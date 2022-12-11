41e47f7a38d5657044d8169ec786eedb6d9fe568
gochigochi <diegoeliseoiovane@gmail.com>
2022-12-11 14:49:55 -0300

    add components to pages

---
+++ pages/conferences.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Conferences = () => {
+  return (
+    <div>conferences</div>
+  )
+}
+
+export default Conferences
---
+++ pages/exhibitors.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Exhibitors = () => {
+  return (
+    <div>exhibitors</div>
+  )
+}
+
+export default Exhibitors
