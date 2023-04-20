bf7bbe8c6a3ecb962a766b2314b3c9e43b10a359
gochigochi <diegoeliseoiovane@gmail.com>
2023-04-20 18:19:02 -0300

    to production poster in video bg

--- components/videoBg/VideoBg.js
+++ components/videoBg/VideoBg.js
@@ -8,7 +8,7 @@ const VideoBack = () => {
     return (
         <>
         {/* autoPlay */}
-            <VideoBg autoPlay loop muted playsInline>
+            <VideoBg autoPlay loop muted playsInline poster='/vide/poster.jpg'>
                 <source src='/video/bg-video.mp4' type='video/mp4' />
             </VideoBg>
             <Overlay />
